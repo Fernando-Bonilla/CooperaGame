25dba584bb9aa7790a42c1343340bcb278e261a3
Fernando <fernandobonilla832@gmail.com>
2025-09-20 17:46:09 -0300

    Initial commit

---
+++ CooperaGame/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CooperaGame.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CooperaGame.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Aca agregar las tablas a mapear
+        public DbSet<Partida> Partidas { get; set; }
+        //public DbSet<Jugador> Jugadores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Partida>()
+                .HasKey(p => p.Id);
+        }
+
+    }
+}
---
+++ CooperaGame/Models/Partida.cs
@@ -0,0 +1,8 @@
+﻿namespace CooperaGame.Models
+{
+    public class Partida
+    {
+        public int Id { get; set; }
+        public string Estado { get; set; } = string.Empty;
+    }
+}
