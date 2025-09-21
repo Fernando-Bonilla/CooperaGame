99ab90582a908002b42f2a04990c2d76ed6aabe5
Fernando <fernandobonilla832@gmail.com>
2025-09-21 14:41:12 -0300

    Agregada Carpeta Models en Test, tmb testeado y funciona todo ok

---
+++ CooperaGame.Tests/Models/PartidaTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CooperaGame;
+using CooperaGame.Models;
+
+namespace CooperaGame.Tests.Models
+{
+    public class PartidaTest
+    {
+
+        [Fact]
+        public void testPruebaPartida()
+        {
+            // Arrange
+            Partida partida = new Partida();
+
+            // Act
+            string resultado = partida.PruebaParaTest();
+
+            //Assert
+            Assert.Equal("Holita", resultado);
+
+        }
+    }
+}
--- CooperaGame/Models/Partida.cs
+++ CooperaGame/Models/Partida.cs
@@ -2,7 +2,18 @@
 {
     public class Partida
     {
+        #region Propiedades
         public int Id { get; set; }
         public string Estado { get; set; } = string.Empty;
+        #endregion Propiedades
+
+
+        #region Metodos
+        public string PruebaParaTest()
+        {
+            return "Holitas";
+        }
+        #endregion Metodos
+
     }
 }
