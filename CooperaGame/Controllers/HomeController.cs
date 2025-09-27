00b84da482f5efe27c618af169c6fcd0710d6f61
Fernando <fernandobonilla832@gmail.com>
2025-09-27 18:37:18 -0300

    Agregada Controladora partidas para ir testeando si se guarda en la BD, Agregado un boton 'Crear Partida' en Home para probar todo esto

--- CooperaGame/Controllers/HomeController.cs
+++ CooperaGame/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using CooperaGame.Data;
 using CooperaGame.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,21 +8,19 @@ namespace CooperaGame.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ CooperaGame/Controllers/PartidasController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CooperaGame.Data;
+using CooperaGame.Models;
+
+namespace CooperaGame.Controllers
+{
+    public class PartidasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartidasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Partidas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Partidas.ToListAsync());
+        }
+
+        // GET: Partidas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var partida = await _context.Partidas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (partida == null)
+            {
+                return NotFound();
+            }
+
+            return View(partida);
+        }
+
+        // GET: Partidas/Create
+        /*public IActionResult Crear()
+        {
+            return View();
+        }*/
+
+        // POST: Partidas/Crear
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear()
+        {
+            Partida partida = new Partida();
+            partida.GenerarMetasRecursos();
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(partida);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index)); ;
+        }
+
+        // GET: Partidas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var partida = await _context.Partidas.FindAsync(id);
+            if (partida == null)
+            {
+                return NotFound();
+            }
+            return View(partida);
+        }
+
+        // POST: Partidas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Estado,CantMadera,CantPiedra,CantComida,Semilla")] Partida partida)
+        {
+            if (id != partida.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(partida);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PartidaExists(partida.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(partida);
+        }
+
+        // GET: Partidas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var partida = await _context.Partidas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (partida == null)
+            {
+                return NotFound();
+            }
+
+            return View(partida);
+        }
+
+        // POST: Partidas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var partida = await _context.Partidas.FindAsync(id);
+            if (partida != null)
+            {
+                _context.Partidas.Remove(partida);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PartidaExists(int id)
+        {
+            return _context.Partidas.Any(e => e.Id == id);
+        }
+    }
+}
