c00f5a58cfc3f6f82d471fdccb76507c68c4adf9
Fernando <fernandobonilla832@gmail.com>
2025-09-28 17:14:12 -0300

    PartidasController: cambié la pisada, ahora con Home, cuando hitea el boton Crear-Partida, reedirige a PartidasController MetodoPadre() que eso chequea, si la ultima partida esta en curso, osea estado != finalizada, reedirige a la vista con esa Partida, de esa forma si se conecta un nuevo jugador y hay una partida en curso se le redirige a esa misma, si el estado de la ultima partida es finalizada, llama a metodo CrearPartida() con sus metas y redirige a esa partida

--- CooperaGame/Controllers/PartidasController.cs
+++ CooperaGame/Controllers/PartidasController.cs
@@ -23,8 +23,7 @@ namespace CooperaGame.Controllers
         public async Task<IActionResult> Index(int id)
         {
             if (id <= 0)
-            {
-                Console.WriteLine($"========== No entra partida null {id}");
+            {                
                 return NotFound();
             }
 
@@ -43,27 +42,88 @@ namespace CooperaGame.Controllers
         // POST: Partidas/Crear
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        /*[HttpPost]        
         public async Task<IActionResult> Crear()
         {
             Partida partida = new Partida();
             partida.GenerarMetasRecursos();
 
-            if (ModelState.IsValid)
+            _context.Add(partida);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", new { id = partida.Id });
+          
+        }*/
+
+        public async Task<int> CrearPartidaAsync()
+        {
+            Partida partida = new Partida();
+            partida.GenerarMetasRecursos();
+
+            try
             {
                 _context.Add(partida);
                 await _context.SaveChangesAsync();
-
-                return RedirectToAction("Index", new {id = partida.Id });
             }
-                        
-            return RedirectToAction("Index", new { id = partida.Id });
-        }       
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }            
+
+            return partida.Id;            
+        }
 
         private bool PartidaExists(int id)
         {
             return _context.Partidas.Any(e => e.Id == id);
+        }
+
+        private async Task<bool> PartidaEnCurso(int id) // Este metodo puede que lo tengamos que mover a ServicePartida
+        {
+            if(id <= 0)
+            {
+                return false;
+            }
+            Partida? partida = await _context.Partidas.FindAsync(id);
+
+            if (partida == null) 
+            {
+                return false;
+            }
+
+            if(partida.Estado != "finalizada")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<int> ObtenerIdUltimaPartida()
+        {
+            Partida partida = await _context.Partidas
+                .OrderByDescending(p => p.Id)
+                .FirstAsync();
+
+            return partida.Id;
+        }
+        
+        public async Task<IActionResult> MetodoPadre()
+        {
+            // Obtengo la ultima partida
+            int idPartida = await ObtenerIdUltimaPartida();
+
+            // Chequeo si esa partida esta en estado jugando
+            if(!await PartidaEnCurso(idPartida)) // Si no tiene estado jugando creo una nueva partida
+            {
+                int idNuevaPartida = await CrearPartidaAsync();
+                return RedirectToAction("Index", new { id = idNuevaPartida });
+            }
+            else // Si tiene estado jugando reedirijo a index con esa partida id
+            {
+               return RedirectToAction("Index", new {id = idPartida});
+            }
+            
         }
     }
 }
