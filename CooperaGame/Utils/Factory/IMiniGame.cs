42011a33480ba5362e53a92043eb6f35ac131ab5
Juan Lluviera <juanlluviera03@gmail.com>
2025-09-28 17:22:31 -0300

    agrega servicio de minijuegos e interfaz de minijuego

---
+++ CooperaGame/Utils/Factory/IMiniGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using CooperaGame.Data;
+using CooperaGame.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+    namespace CooperaGame.Utils.Factory
+    {
+        public interface IMiniGame
+        {
+            public IActionResult MostrarJuego();
+
+        }
+    }
---
+++ CooperaGame/Utils/MiniGameServicies.cs
@@ -0,0 +1,21 @@
+﻿namespace CooperaGame.Utils
+{
+    public sealed  class MiniGameServicies //usamos patron singleton para asegurarnos que solo exista una instancia del servicio
+    {
+        private static MiniGameServicies? instance = null;
+
+        private MiniGameServicies()
+        {
+
+        }
+
+        public static MiniGameServicies GetServiceInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MiniGameServicies();
+            }
+            return instance;
+        }
+    }
+}
