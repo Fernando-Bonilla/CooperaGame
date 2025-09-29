4c3598efd23be70e90b4a42e321a54ef6776bb43
Fernando <fernandobonilla832@gmail.com>
2025-09-28 21:07:14 -0300

    Agregada carpeta Service, y dentro PartidaService con algunos metodos, removido metodos de JugadoresController, falta hacer test para los metodos en la controladoraPartida y para estos metodos en PartidaService

---
+++ CooperaGame.Tests/Controllers/PartidaControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CooperaGame.Tests.Controllers
+{
+    internal class PartidaControllerTest
+    {
+        // Hacer los test para los metodos en controladora partida
+    }
+}
---
+++ CooperaGame/Services/PartidaService.cs
@@ -0,0 +1,40 @@
+﻿using CooperaGame.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CooperaGame.Services
+{
+    public class PartidaService
+    {
+        private readonly ApplicationDbContext _context;
+        public PartidaService(ApplicationDbContext context) 
+        { 
+            _context = context;
+        }
+
+        // metodos que voy a precisar
+
+        // uno que me diga cuanto va recolectado de cada recurso
+        public async Task<int> cantidadRecursoRecoletado(int idPartida, string recurso)
+        {          
+            int cantidad = await _context.Recolecciones
+                .Where(r => r.PartidaId == idPartida && r.Recurso == recurso)
+                .CountAsync();
+
+            return cantidad;
+        }
+
+        /*public async int metaRecurso(int idPartida, string recurso)
+        {
+
+        }*/
+
+        // uno que chequee la cantidad y lo compare con la meta
+        public bool metaRecursoAlcanzada(int idPartida, string recurso)
+        {
+            
+            return false;
+        }
+
+    }
+}
