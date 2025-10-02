51a2efdf4dff687befe38593963819f490dc7091
Juan Lluviera <juanlluviera03@gmail.com>
2025-10-02 17:12:30 -0300

    Agrega servicio usando localstorage del lado del cliente para mayor rendimiento, con funciones para crear, borrar y mostrar. En caso de que no haya usuario en partida/index, no muestra boton para agregar material, de lo contrario si.

---
+++ CooperaGame/wwwroot/js/UserServices.js
@@ -0,0 +1,22 @@
+﻿function guardarUsuario(usuario) {
+    if (usuario == null) {
+        return false;
+    }
+    try {
+        localStorage.setItem("user",usuario);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    };
+};
+
+function getUsuario() {
+    const user = localStorage.getItem("user");
+    
+    return user;
+};
+
+function deleteUsuario() {
+    localStorage.setItem("user", null);
+};
